49aa14e13f3abb65eef6297d2ce02b4cddc9f738
anmoljain16 <ajanmoldosi@gmail.com>
2023-02-03 14:08:56 +0530

    Created get request and post request

--- GoFiber/Main.go
+++ GoFiber/Main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import "github.com/gofiber/fiber/v2"
 
+type Fullname struct {
+	FirstName string
+	LastName  string
+}
+
+func getAnmol(ctx *fiber.Ctx) error {
+
+	return ctx.Status(fiber.StatusOK).JSON(&fname)
+}
+
+var fname Fullname
+
+func postAnmol(ctx *fiber.Ctx) error {
+	body := new(Fullname)
+	err := ctx.BodyParser(body)
+	if err != nil {
+		ctx.Status(fiber.StatusBadGateway).SendString(err.Error())
+		return err
+	}
+	fname = Fullname{
+		FirstName: body.FirstName,
+		LastName:  body.LastName,
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fname)
+}
+
+func main() {
+	app := fiber.New()
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello World")
+	})
+
+	app.Get("/anmol", getAnmol)
+	app.Post("/anmol", postAnmol)
+	app.Listen(":80")
 }
